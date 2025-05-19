38d5bddae4e63153c1ef4a02571c5106a893b0ed
Themelis Soulounias <142552158+TheSoulounias@users.noreply.github.com>
2025-05-18 20:12:56 -0400

    Add math logic

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(a,b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b;	
 };
 
-const sum = function() {
-	
+const sum = function(a) {
+	a.reduce((previous, current) => {
+	 return previous + current;
+	}, initial)
+  return initial
 };
 
-const multiply = function() {
-
+const multiply = function(a,b) {
+  return a * b;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+  return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	let i = 0
+  let j = 0
+  let k = 0
+  while (a < 0) {
+    j = a;
+    a--;
+    k = j*a;
+    i+=k;
+  }
+  return i;
 };
 
 // Do not edit below this line
